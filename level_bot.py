d0a33972870c33bec289f54116ead540e548d235
moroskin321 <pilaop900@gmail.com>
2020-10-23 20:27:40 +0300

    ...

--- level_bot.py
+++ level_bot.py
@@ -4,12 +4,12 @@ from Question import *
 quiz_work = False
 lvl = {}
 
-token = "NzU1NzM0OTMyNTY3MDk3MzQ0.X2HnAA.nlwlRXSg9ZU4APeMI6bhZgybduQ"
+token = " "
 bot = commands.Bot(command_prefix = '!')
 
 q1 = Question('Что за блок?', './Pictures/0.png', 'почва душ')
 q2 = Question('Что получится?', './Pictures/1.png', 'фонарь душ')
-q = [q1, q2]
+q = []
 qn = 0
 def get_levels():
     file = open('Levels.txt', 'r')
@@ -58,44 +58,84 @@ async def start(ctx):
     lvl[str(ctx.author)] = 1
     save_levels()
 
-@bot.command()
+@bot.command(brief = "Только 'Глава мафии' задать уровень участника",
+description = "Только 'Глава мафии' задать уровень участника")
 async def set_level(ctx, name, level):
     if is_admin(ctx):
+        if not level.isdigit():
+            await ctx.channel.send('Ошибка 2: Неверный формат(Уровень должен быть числом)')
+            return
         lvl[name] = int(level)
         await ctx.channel.send('У персонажа ' + name + ' уровень ' + str(lvl[name]))
     else:
         await ctx.channel.send('Ошибка 1: Недостаточно прав')
     save_levels()
 
-@bot.command()
+@bot.command(brief = "Только 'Глава мафии' изменение уровня участника",
+description = "Только 'Глава мафии' изменение уровня участника")
 async def level_up(ctx, name, level):
     if is_admin(ctx):
-        lvl[name] += int(level)
-        await ctx.channel.send('У персонажа ' + name + ' уровень ' + str(lvl[name]))
+        if not level.isdigit():
+            await ctx.channel.send('Ошибка 2: Неверный формат(Уровень должен быть числом)')
+            return
+        if lvl.get(name) != None:
+            lvl[name] += int(level)
+            await ctx.channel.send('У персонажа ' + name + ' уровень ' + str(lvl[name]))
+        else:
+            lvl[name] = 1
+            lvl[name] += int(level)
+            await ctx.channel.send('У персонажа ' + name + ' уровень ' + str(lvl[name]))
     else:
         await ctx.channel.send('Ошибка 1: Недостаточно прав')
     save_levels()
 
 
 def level_up(name):
-    lvl[name] += 1
+    if lvl.get(name) != None:
+        lvl[name] += 1
+    else:
+        lvl[name] = 2
     save_levels()
 
+async def check_end(ctx):
+    global q
+    global qn
+    if qn >= len(q):
+        qn = 0
+        await ctx.channel.send('викторина сброшена')
 
-@bot.command()
-async def Admin_menu(ctx):
-    if is_admin(ctx):
-        lvl[str(ctx.author)] = 'Admin'
-
-@bot.command()
+@bot.command(brief = "Только 'Глава мафии' старт викторины",
+description = "Только 'Глава мафии' старт викторины")
 async def ask(ctx):
     global quiz_work
     if is_admin(ctx):
-        quiz_work = True
-        global qn
         global q
-        await ctx.channel.send(q[qn].text)
-        await ctx.channel.send(file = discord.File(q[qn].image_path))
+        if len(q) > 0:
+            await check_end(ctx)
+            quiz_work = True
+            global qn
+            question = discord.Embed(
+                title = q[qn].text,
+                colour = discord.Colour.red())
+    #        question.set_image(discord.File(q[qn].image_path))
+            await ctx.send(embed = question, file = discord.File(q[qn].image_path))
+        else:
+            await ctx.channel.send('Ошибка 4: Нет вопросов, просьба обратится к администратору.')
+    else:
+        await ctx.channel.send('Ошибка 1: Недостаточно прав')
+#МЫ СТРАДАЛИ ВСЕ ВРЕМЯ И ВСЕГДА
+@bot.command(brief = "Только 'Глава мафии' пропуск вопроса викторины",
+description = "Только 'Глава мафии' пропуск вопроса викторины")
+async def skip(ctx):
+    if is_admin(ctx):
+        global quiz_work
+        if quiz_work:
+            global qn
+            quiz_work = False
+            qn += 1
+            await ctx.send('вопрос пропущен, !ask для следующего вопроса')
+        else:
+            await ctx.channel.send('Ошибка 3: Нет вопроса для пропуска')
     else:
         await ctx.channel.send('Ошибка 1: Недостаточно прав')
 
@@ -111,6 +151,7 @@ async def on_message(ctx):
             await ctx.channel.send('У персонажа ' + str(ctx.author) + ' уровень ' + str(lvl[str(ctx.author)]))
             quiz_work = False
             qn += 1
+            await check_end(ctx)
     await bot.process_commands(ctx)
 
 bot.run(token)
